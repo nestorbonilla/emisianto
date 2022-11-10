bce63933bf032218de85c734174ca7c66e238fe8
soloseng <102702451+soloseng@users.noreply.github.com>
2022-11-10 14:38:43 -0500

    log verification request response

--- services/twilio.ts
+++ services/twilio.ts
@@ -14,6 +14,7 @@ export async function sendSmsVerificationToken(phoneNumber: string) {
       body: data,
     }
   );
+  console.log("Verification request response:", response);
 }
 
 export async function verifyToken(
